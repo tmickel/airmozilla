7ac4e530d09efae3fea7988535081cbacdae0630
Tim Mickel <tmickel@mit.edu>
2012-06-12 10:15:02 -0700

    reorganization and add group manager

--- airmozilla/manage/forms.py
+++ airmozilla/manage/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.contrib.auth.forms import UserChangeForm
 
 class UserEditForm(UserChangeForm):
@@ -13,3 +13,14 @@ class UserEditForm(UserChangeForm):
         model = User
         fields = ('is_active', 'is_staff', 'is_superuser', 
                   'groups', 'user_permissions')
+
+class GroupEditForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(GroupEditForm, self).__init__(*args, **kwargs)
+        self.fields['name'].required = True
+        choices = self.fields['permissions'].choices
+        self.fields['permissions'] = forms.MultipleChoiceField(choices=choices,
+                                            widget=forms.CheckboxSelectMultiple,
+                                            required=False)
+    class Meta:
+        model = Group
