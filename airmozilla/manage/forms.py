3337893b79539d296dd672ac8c768921f5211ae3
Tim Mickel <tmickel@mit.edu>
2012-06-12 11:37:32 -0700

    fixes form ridiculousness

--- airmozilla/manage/forms.py
+++ airmozilla/manage/forms.py
@@ -1,15 +1,8 @@
 from django import forms
-
 from django.contrib.auth.models import User, Group
-from django.contrib.auth.forms import UserChangeForm
 
 
-class UserEditForm(UserChangeForm):
-    def __init__(self, *args, **kwargs):
-        super(UserEditForm, self).__init__(*args, **kwargs)
-        self.fields.pop('username')
-        self.fields.pop('password')
-
+class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('is_active', 'is_staff', 'is_superuser',
