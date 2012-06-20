56a26220cb57143f1b074c1628f3a6dc92fb1f00
Tim Mickel <tmickel@mit.edu>
2012-06-20 16:30:42 -0700

    Style and performance improvements per code review.

--- airmozilla/main/helpers.py
+++ airmozilla/main/helpers.py
@@ -4,12 +4,12 @@ from jingo import register
 
 
 @register.filter
-def js_date(datetime, format='ddd, MMM D, YYYY, h:mma UTCZZ'):
+def js_date(dt, format='ddd, MMM D, YYYY, h:mma UTCZZ'):
     """ Python datetime to a time tag with JS Date.parse-parseable format. """
-    date = '%i/%i/%i' % (datetime.month, datetime.day, datetime.year)
-    time = str(datetime.time())
-    tz = datetime.tzname()  # Should always be UTC
-    formatted_datetime = ' '.join([date, time, tz])
+    dt_date = dt.strftime('%m/%d/%Y')
+    dt_time = str(dt.time())
+    dt_tz = dt.tzname()  # Should always be UTC
+    formatted_datetime = ' '.join([dt_date, dt_time, dt_tz])
     return jinja2.Markup('<time datetime="%s" class="jstime" \
                            data-format="%s">%s</time>'
                  % (formatted_datetime, format, formatted_datetime))
--- airmozilla/main/static/main/js/timezone.js
+++ airmozilla/main/static/main/js/timezone.js
@@ -2,9 +2,10 @@ $(function() {
     'use strict';
     $('time.jstime').each(function(i, time) {
         // Find all relevant <time> elements and replace with formatted time.
-        var datetime = $(time).attr('datetime');
-        var format = $(time).attr('data-format');
+        var $element = $(time);
+        var datetime = $element.attr('datetime');
+        var format = $element.attr('data-format');
         var parsed = moment(datetime);
-        $(time).text(parsed.format(format));
+        $element.text(parsed.format(format));
     });
 });
