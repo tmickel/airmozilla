61919a43dcb8d62fb4e49409920f073bba7a3daa
Tim Mickel <tmickel@mit.edu>
2012-07-02 13:59:53 -0700

    Better path joining for _upload_path.

--- airmozilla/main/models.py
+++ airmozilla/main/models.py
@@ -8,11 +8,12 @@ from django.db import models
 def _upload_path(tag):
     def _upload_path_tagged(instance, filename):
         now = datetime.datetime.now()
-        path = now.strftime('%Y/%m/%d/')
-        hashed_filename = (hashlib.md5(filename + 
+        path = os.path.join(now.strftime('%Y'), now.strftime('%m'),
+                            now.strftime('%d'))
+        hashed_filename = (hashlib.md5(filename +
                             str(now.microsecond)).hexdigest())
         __, extension = os.path.splitext(filename)
-        return tag + '/' + path + hashed_filename + extension
+        return os.path.join(tag, path, hashed_filename + extension)
     return _upload_path_tagged
 
 
