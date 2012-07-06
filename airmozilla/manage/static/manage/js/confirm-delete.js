4d53c031bfb52bb398390bc0e6627716a14b93dc
Tim Mickel <tmickel@mit.edu>
2012-07-06 16:00:01 -0700

    style fix on confirm dialogs

--- airmozilla/manage/static/manage/js/confirm-delete.js
+++ airmozilla/manage/static/manage/js/confirm-delete.js
@@ -2,9 +2,6 @@ $(function() {
     'use strict';
     // Pop a confirm dialogue on delete button submission.
     $('.confirm').submit(function() {
-        if (confirm('Are you sure you want to remove this item?')) {
-            return true;
-        }
-        return false;
+        return confirm('Are you sure you want to remove this item?');
     });
 });
