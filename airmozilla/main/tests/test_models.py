c7c68db6e7d8e16bbbe455b570cc6be94b86fd6b
Tim Mickel <tmickel@mit.edu>
2012-07-24 14:10:00 -0700

    Remove unneccesary _fixture_event

--- airmozilla/main/tests/test_models.py
+++ airmozilla/main/tests/test_models.py
@@ -9,12 +9,6 @@ from airmozilla.main.models import Approval, Event, EventOldSlug
 class ForeignKeyTests(TestCase):
     fixtures = ['airmozilla/manage/tests/main_testdata.json']
 
-    def _fixture_event(self):
-        """Return a fully-featured event from the fixtures."""
-        event = Event.objects.get(id=22)
-        ok_(event)
-        return event
-
     def _successful_delete(self, obj):
         """Delete an object and ensure it's deleted."""
         model = obj.__class__
@@ -33,25 +27,25 @@ class ForeignKeyTests(TestCase):
 
     def test_template_remove(self):
         """Deleting a Template does not delete associated Event."""
-        event = self._fixture_event()
+        event = Event.objects.get(id=22)
         self._successful_delete(event.template)
         self._refresh_ok(event)
 
     def test_location_remove(self):
         """Deleting a Location does not delete associated Event."""
-        event = self._fixture_event()
+        event = Event.objects.get(id=22)
         self._successful_delete(event.location)
         self._refresh_ok(event)
 
     def test_category_remove(self):
         """Deleting a Category does not delete associated Event."""
-        event = self._fixture_event()
+        event = Event.objects.get(id=22)
         self._successful_delete(event.category)
         self._refresh_ok(event)
 
     def test_user_creator_remove(self):
         """Deleting a creator User does not delete associated Event."""
-        event = self._fixture_event()
+        event = Event.objects.get(id=22)
         user = User.objects.get(id=1)
         event.creator = user
         event.modified_user = None
@@ -61,7 +55,7 @@ class ForeignKeyTests(TestCase):
 
     def test_user_modifier_remove(self):
         """Deleting a modifying User does not delete associated Event."""
-        event = self._fixture_event()
+        event = Event.objects.get(id=22)
         user = User.objects.get(id=1)
         event.creator = None
         event.modified_user = user
@@ -71,7 +65,7 @@ class ForeignKeyTests(TestCase):
 
     def test_eventoldslug_remove(self):
         """Deleting an EventOldSlug does not delete associated Event."""
-        event = self._fixture_event()
+        event = Event.objects.get(id=22)
         oldslug = EventOldSlug.objects.get(id=1)
         ok_(oldslug)
         eq_(oldslug.event, event)
@@ -80,7 +74,7 @@ class ForeignKeyTests(TestCase):
 
     def test_group_remove(self):
         """Deleting a Group does not delete associated Approval."""
-        event = self._fixture_event()
+        event = Event.objects.get(id=22)
         group = Group.objects.get(id=1)
         approval = Approval(event=event, group=group)
         approval.save()
@@ -89,7 +83,7 @@ class ForeignKeyTests(TestCase):
 
     def test_user_remove(self):
         """Deleting a User does not delete associated Approval."""
-        event = self._fixture_event()
+        event = Event.objects.get(id=22)
         user = User.objects.get(id=1)
         approval = Approval(event=event, user=user)
         approval.save()
@@ -98,7 +92,7 @@ class ForeignKeyTests(TestCase):
 
     def test_approval_remove(self):
         """Deleting an Approval does not delete associated Event."""
-        event = self._fixture_event()
+        event = Event.objects.get(id=22)
         approval = Approval(event=event)
         approval.save()
         self._successful_delete(approval)
@@ -106,25 +100,25 @@ class ForeignKeyTests(TestCase):
 
     def test_participants_remove(self):
         """Deleting all Participants does not delete associated Event."""
-        event = self._fixture_event()
+        event = Event.objects.get(id=22)
         participants = event.participants.all()
-        ok_(len(participants) > 0)
+        ok_(participants.exists())
         for participant in participants:
             self._successful_delete(participant)
         self._refresh_ok(event)
 
     def test_tags_remove(self):
         """Deleting all Tags does not delete associated Event."""
-        event = self._fixture_event()
+        event = Event.objects.get(id=22)
         tags = event.tags.all()
-        ok_(len(tags) > 0)
+        ok_(tags.exists())
         for tag in tags:
             self._successful_delete(tag)
         self._refresh_ok(event)
 
     def test_event_remove_approval(self):
         """Deleting an Event DOES remove associated Approval."""
-        event = self._fixture_event()
+        event = Event.objects.get(id=22)
         approval = Approval(event=event)
         approval.save()
         self._successful_delete(event)
@@ -132,7 +126,7 @@ class ForeignKeyTests(TestCase):
 
     def test_event_remove_eventoldslug(self):
         """Deleting an Event DOES remove associated EventOldSlug."""
-        event = self._fixture_event()
+        event = Event.objects.get(id=22)
         oldslug = EventOldSlug.objects.get(id=1)
         eq_(oldslug.event, event)
         self._successful_delete(event)
