32d4160206b627ce898ca84126f92232d8a225d4
Tim Mickel <tmickel@mit.edu>
2012-08-07 14:15:21 -0700

    Fix app permissions on manage/context_processors

--- airmozilla/manage/context_processors.py
+++ airmozilla/manage/context_processors.py
@@ -6,12 +6,12 @@ def badges(request):
         return {}
     context = {'badges': {}}
     # Event manager badge for unprocessed events
-    if request.user.has_perm('change_event_others'):
+    if request.user.has_perm('main.change_event_others'):
         events = Event.objects.initiated().count()
         if events > 0:
             context['badges']['events'] = events
     # Approval inbox badge
-    if request.user.has_perm('change_approval'):
+    if request.user.has_perm('main.change_approval'):
         approvals = Approval.objects.filter(
             group__in=request.user.groups.all(),
             processed=False
@@ -19,7 +19,7 @@ def badges(request):
         if approvals > 0:
             context['badges']['approvals'] = approvals
     # Uncleared participants badge
-    if request.user.has_perm('change_participant_others'):
+    if request.user.has_perm('main.change_participant_others'):
         participants = (
             Participant.objects.filter(cleared=Participant.CLEARED_NO).count()
         )
